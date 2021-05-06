89b3e609c786c24fc10e05c46405b4d74b5b05dc
kmarius <froase@gmail.com>
2021-05-06 20:58:46 +0200

    remove logging

--- nav.go
+++ nav.go
@@ -447,7 +447,6 @@ func newNav(height int) *nav {
 			path := filepath.Dir(ev.Path())
 			now := time.Now()
 			next := now
-			log.Printf("event: %s", ev.Event())
 			force := false
 			switch ev.Event() {
 			case notify.InModify, notify.InCloseWrite:
